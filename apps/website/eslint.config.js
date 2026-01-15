310983db0d6c48b42386aa01a1b680cc891c2b4c
Paul <paul@askphill.com>
2026-01-15 11:11:21 +0100

    fix(lint): explicit jest version for eslint-plugin-jest

--- apps/website/eslint.config.js
+++ apps/website/eslint.config.js
@@ -1,4 +1,4 @@
-import {fixupConfigRules, fixupPluginRules} from '@eslint/compat';
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
 import eslintComments from 'eslint-plugin-eslint-comments';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
@@ -9,9 +9,9 @@ import _import from 'eslint-plugin-import';
 import tsParser from '@typescript-eslint/parser';
 import jest from 'eslint-plugin-jest';
 import path from 'node:path';
-import {fileURLToPath} from 'node:url';
+import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
-import {FlatCompat} from '@eslint/eslintrc';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -234,6 +234,11 @@ export default [
       globals: {
         ...globals.node,
         ...globals.jest,
+      },
+    },
+    settings: {
+      jest: {
+        version: 29,
       },
     },
   },
